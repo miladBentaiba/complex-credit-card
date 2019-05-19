8342fcf2949d0c59fdaffa64a00b4350bf926f39
miladBentaiba <bm_bentaiba@esi.dz>
2019-05-19 05:57:33 +0100

    change functional component of app into class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import Card from './Card'
 
-function App() {
-  return ( 
-  <div className="parent">
-    <Card/>
-    <div className="inputs">
-      <input type="number"/>
-      <input type="text"/>
-      <input type=""/>
-    </div>
-  </div>
-  
-  )
+export default class App extends React.Component{
+  render(){
+    return ( 
+      <div className="parent">
+        <Card/>
+        <div className="inputs">
+          <input type="number" maxlength="14" placeholder="card number"/>
+          <input type="text" maxlength="20" placeholder="full name"/>
+          <input type="text" maxlength="5" placeholder="expiration date"/>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
