801d178539572dee4c133d0ecfe1d01d310b1591
miladBentaiba <bm_bentaiba@esi.dz>
2019-05-19 05:58:28 +0100

    change functional component of card into class

--- src/Card.js
+++ src/Card.js
@@ -2,30 +2,31 @@ import React from 'react';
 import visa from './visa.png'
 import sim from './chips.png'
 
-function Card() {
-  return (
-  <div className="credit-card-wrap">
-    <div className="mk-icon-world-map"></div>
-    <div className="credit-card-inner">
-      <header className="header">
-        <div className="credit-logo">
-          <div className="shape"><span className="txt">PB</span></div> <span className="text">Public Bank of Tunisia</span>
-        </div>
-      </header>
-      <div className="mk-icon-sim"><img alt="sim" src={sim} width="55px" height="40px"/></div>
-      <div className="credit-font credit-card-number" data-text="4716">4716 6109 5211 3010</div>
-      <footer className="footer">
-        <div className="clearfix">
-          <div className="pull-left">
-            <div className="credit-card-date"><span className="title">Expires End</span><span className="credit-font">01/018</span></div>
-            <div className="credit-font credit-author">MOHAN KHADKA</div>
+export default class Card extends React.Component{
+  render() {
+    return (
+    <div className="credit-card-wrap">
+      <div className="mk-icon-world-map"></div>
+      <div className="credit-card-inner">
+        <header className="header">
+          <div className="credit-logo">
+            <div className="shape"><span className="txt">PB</span></div> <span className="text">Public Bank of Tunisia</span>
           </div>
-          <div className="pull-right"><img src={visa} alt="visa" className="mk-icon-visa"/></div>
-        </div>
-      </footer>
+        </header>
+        <div className="mk-icon-sim"><img alt="sim" src={sim} width="55px" height="40px"/></div>
+        <div className="credit-font credit-card-number" data-text="4716">4716 6109 5211 3010</div>
+        <footer className="footer">
+          <div className="clearfix">
+            <div className="pull-left">
+              <div className="credit-card-date"><span className="title">Expires End</span><span className="credit-font">01/018</span></div>
+              <div className="credit-font credit-author">MOHAN KHADKA</div>
+            </div>
+            <div className="pull-right"><img src={visa} alt="visa" className="mk-icon-visa"/></div>
+          </div>
+        </footer>
+      </div>
     </div>
-  </div>
-  );
+    );
+  }
 }
 
-export default Card;
